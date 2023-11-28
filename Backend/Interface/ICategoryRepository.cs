bf71fbb17a2674a705e7319c320da229f8277fdc
Ivan Corlli <corlliivan@gmail.com>
2023-11-28 18:47:46 -0300

    [Refactor] Category interface changed, now it checks if the name of category habeen created before

--- Backend/Interface/ICategoryRepository.cs
+++ Backend/Interface/ICategoryRepository.cs
@@ -1,8 +1,6 @@
-using Backend.Entity;
-
 namespace Backend.Interface;
 
 public interface ICategoryRepository
 {
-  public void Create(Category category);
+  public void IsNameUsed(string Name);
 }
--- Backend/Interface/IRecipeRepository.cs
+++
@@ -1,8 +0,0 @@
-using Backend.Entity;
-
-namespace Backend.Interface;
-
-public interface IRecipeRepository
-{
-  public void Create(Recipe Recipe);
-}
